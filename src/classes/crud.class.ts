89361377880eb22898192759e0ccf1a4b7d01e60
MohammedAl-rowad <rowadwo@gmail.com>
2019-06-04 01:28:22 +0300

    vid 4

--- src/classes/crud.class.ts
+++ src/classes/crud.class.ts
@@ -1,4 +1,5 @@
 import { prmisifyTheQuery } from '../helpers/util.helper';
+import { WhereConditions } from '../interfaces/interfaces';
 
 export function Crud() {}
 
@@ -20,6 +21,40 @@ Crud.prototype.create = function(data: any): Promise<any> {
   );
 };
 
+// UPDATE {TABLE_NAME} SET values... WHERE conditions..
+Crud.prototype.update = function(
+  newDate: any,
+  whereObj: WhereConditions
+): Promise<any> {
+  let setPart = 'SET ';
+  const preparedArray: Array<string> = [];
+  loopThroughObj(newDate, (key, value) => {
+    setPart = setPart.concat(`${key} = ?`);
+    preparedArray.push(value);
+  });
+  const wherePart = extractWhereStmt(whereObj, val => {
+    preparedArray.push(val);
+  });
+  const q = `UPDATE ${this.name} ${setPart} ${wherePart}`;
+  console.log(q, preparedArray);
+  return prmisifyTheQuery(q, preparedArray);
+};
+
+const extractWhereStmt = (
+  whereObj: WhereConditions,
+  cb: (val: any) => void
+): string => {
+  let wherePart = '';
+  loopThroughObj(whereObj, colName => {
+    const { opertation, value } = whereObj[colName];
+    wherePart = wherePart.concat(
+      `${opertation ? opertation : ''} ${colName} = ? `
+    );
+    cb(value);
+  });
+  return wherePart ? `WHERE ${wherePart}` : '';
+};
+
 const loopThroughObj = (obj: any, cb: (key: string, values: any) => any) => {
   for (const key in obj) {
     if (obj.hasOwnProperty(key)) {
@@ -27,3 +62,13 @@ const loopThroughObj = (obj: any, cb: (key: string, values: any) => any) => {
     }
   }
 };
+
+const test = new (Crud as any)();
+
+test.update(
+  { name: 'rowad' },
+  {
+    id: { value: 1 },
+    name: { value: 'polo', opertation: 'AND' }
+  }
+);
